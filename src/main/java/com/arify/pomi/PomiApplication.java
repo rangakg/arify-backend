5690d9979e02042366ccab6d9b8a2e3fa703c954
suhs <ranga,kg@gmail.co>
2026-02-23 21:25:22 +0530

    Initial Spring Boot 4 setup - Java 21

---
+++ src/main/java/com/arify/pomi/PomiApplication.java
@@ -0,0 +1,13 @@
+package com.arify.pomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomiApplication.class, args);
+	}
+
+}
