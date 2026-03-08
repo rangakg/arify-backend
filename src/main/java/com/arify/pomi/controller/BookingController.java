d5be33dd69290421b95e92151e4b73afd0b8cde9
suhs <ranga,kg@gmail.co>
2026-03-08 22:12:31 +0530

    updated controllers

--- src/main/java/com/arify/pomi/controller/BookingController.java
+++ src/main/java/com/arify/pomi/controller/BookingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/booking")
 public class BookingController {
 
     private final BookingService bookingService;
